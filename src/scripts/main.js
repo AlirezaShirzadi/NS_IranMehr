ca58d52707a0cfe6ebf7e9669ae1e69383c9dc7e
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-05-26 16:14:46 +0330

    Add "About" section with styles, scripts, and assets
    Introduce a new "About" section on the homepage, including content, styles, and images. Integrate Fancybox library for interactive media functionality and update dependencies accordingly. Adjust home slider styling for better alignment.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 /* components */
 import "./components/menu.js"
 import "./components/slider.js"
+
+/* libraries */
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox]", {
+
+});
