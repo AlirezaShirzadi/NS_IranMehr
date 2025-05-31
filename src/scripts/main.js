2fb61d1a8ac8bb36435ee31d8b19ab89e28c31e5
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-05-31 10:30:04 +0330

    Add "Go to Top" button and footer with social links
    Introduced a "Go to Top" button with smooth scrolling functionality. Added a comprehensive footer section featuring academy information, contact details, social media links, and app download options. Updated assets and included new images. Adjusted SVG usage and fixed minor formatting inconsistencies in the markup.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,13 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
 Fancybox.bind("[data-fancybox]", {
 
 });
+
+/* goto top */
+$(document).ready(function () {
+    $('#goto-top').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+})
