fc3cd889bba03ec96475f5d686cc594bfbdabc8b
Alireza Shirzadi <shirzadiwork@gmail.com>
2025-06-10 09:25:34 +0330

    Update contact form validation and cleanup unused scripts
    Re-added `jquery-validation` in `contact.html` for form validation and removed its unused import from `main.js`. Updated fetch token for secure workflow invocation.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,10 @@ import "./components/slider.js"
 import "./components/tabs.js"
 
 /* libraries */
-import $ from 'jquery';
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import 'jquery-validation';
 
 AOS.init({
     disable: "mobile",
